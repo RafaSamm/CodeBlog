1a45a67c8217a7f87de0ec05c210c9f19636cbd8
RafaSamm <rafarhs286@gmail.com>
2023-09-25 19:21:39 -0300

    atualizando a camada controller

--- src/main/java/br/com/rhssolutions/codeblog/controller/PostController.java
+++ src/main/java/br/com/rhssolutions/codeblog/controller/PostController.java
@@ -4,6 +4,7 @@ import br.com.rhssolutions.codeblog.model.Post;
 import br.com.rhssolutions.codeblog.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,17 +17,24 @@ public class PostController {
     private PostService postService;
 
     public PostController(PostService postService) {
-         this.postService = postService;
+        this.postService = postService;
     }
-    @RequestMapping(value = "/posts" ,method = RequestMethod.GET)
-    public ModelAndView getPosts(){
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public ModelAndView getPosts() {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = postService.findAll();
         mv.addObject("posts", posts);
         return mv;
     }
 
-
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = postService.findById(id);
+        mv.addObject("post", post);
+        return mv;
+    }
 
 
 }
