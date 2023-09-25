4303d3f2dcd83823d6da782b6f6adf455ef35dd9
RafaSamm <rafarhs286@gmail.com>
2023-09-25 09:18:07 -0300

    ajustes nas camadas service, repository, e implementações

---
+++ src/main/java/br/com/rhssolutions/codeblog/service/serviceImpl/PostServiceImpl.java
@@ -0,0 +1,29 @@
+package br.com.rhssolutions.codeblog.service.serviceImpl;
+
+import br.com.rhssolutions.codeblog.model.Post;
+import br.com.rhssolutions.codeblog.repository.PostRepository;
+import br.com.rhssolutions.codeblog.service.PostService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    @Override
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return postRepository.findById(id).get();
+
+
+    }
+    @Override
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+}
