b41aad6430bcf5fbc029fa04de30bdd5e779e283
RafaSamm <rafarhs286@gmail.com>
2023-09-24 21:57:39 -0300

    Definindo algumas camadas, ajustes e dependências

---
+++ src/main/java/br/com/rhssolutions/codeblog/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package br.com.rhssolutions.codeblog.repository;
+
+import br.com.rhssolutions.codeblog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
